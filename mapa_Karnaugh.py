eef5fa73e2eea68e6f26ee5e6f6cb616bd200cae
blueisas <blueisas@outlook.com>
2024-04-17 23:51:41 -0300

    colorcode yay

--- mapa_Karnaugh.py
+++ mapa_Karnaugh.py
@@ -1,3 +1,17 @@
+from itertools import product
 
 
+def tabela_verdade(vin):  # todas as combinações possíveis de 0 e 1 len(variables) vezes
+    tabela = list(product([0, 1], repeat=len(vin)))
+    return tabela
 
+
+def mapa_karnaugh(vin, func):
+    tabela = tabela_verdade(vin)
+    t = [[func(*binario) for binario in linha] for linha in tabela]
+
+
+vin = ['a', 'b']
+func = lambda a, b: a and not b
+
+print(tabela_verdade(vin))
---
+++ resistor_colorcode.py
@@ -0,0 +1,36 @@
+def colorcode(cor):
+    cores = (
+        ('prata', -2),
+        ('dourado', -1),
+        ('preto', 0),
+        ('marrom', 1),
+        ('vermelho', 2),
+        ('laranja', 3),
+        ('amarelo', 4),
+        ('verde', 5),
+        ('azul', 6),
+        ('roxo', 7),
+        ('cinza', 8),
+        ('branco', 9)
+    )
+
+    resistor = 0
+    multiplicador = 1
+    quantidade = int(len(cor))
+
+    # cálculo das faixas genéricas
+    for cor_nome, cor_valor in cores[2:]:  # percorre a tupla (  ,  ) excluindo a possibilidade de dourado e prata
+        if cor_nome in cor:  # quando encontrar na lista a cor inserida pelo usuário, calcula e coontinua procurando
+            resistor = (resistor * 10) + cor_valor
+
+    # cálculo da última faixa (multiplicador)
+    for cor_nome, cor_valor in cores:
+        if cor[-1] == cor_nome:
+            multiplicador = 10 ** cor_valor
+            break
+    resistor *= multiplicador
+    return resistor
+
+
+cores = ['vermelho', 'verde', 'prata']  # teste
+print("O valor do resistor é:", colorcode(cores), "ohms")
